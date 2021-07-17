f88ccb0ba4900d3ed717b7b8e2c55801f8e3da6a
admin <1>
2021-07-17 11:10:03 +0800

    Default Changelist 测试全球汽车

--- ZX/src/com/company/Main.java
+++ ZX/src/com/company/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("测试数据");
-        System.out.println("11111111111111" +
-                "11111111从v1111");
+        System.out.println("1");
     }
 }
